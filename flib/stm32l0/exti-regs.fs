6dc4d2bd9545fab5a32a82267ca2416a1634da41
flabbergast <flabbergast@drak.xyz>
2019-07-28 17:40:52 +0100

    Add l0/exti-regs file.

---
+++ flib/stm32l0/exti-regs.fs
@@ -0,0 +1,8 @@
+$40010400 constant EXTI
+     EXTI $00 + constant EXTI-IMR
+     EXTI $04 + constant EXTI-EMR
+     EXTI $08 + constant EXTI-RTSR
+     EXTI $0C + constant EXTI-FTSR
+     EXTI $10 + constant EXTI-SWIER
+     EXTI $14 + constant EXTI-PR
+
