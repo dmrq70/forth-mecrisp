a720de9ebbcef4cd6949ff2d2d3ff91f945e4ad5
flabbergast <flabbergast@drak.xyz>
2019-08-07 09:09:05 +0100

    Regenerate templated fs with -i.

--- msp430/flib/fr2476/i2c-bb-base.fs
+++ msp430/flib/fr2476/i2c-bb-base.fs
@@ -1,38 +1,38 @@
-\ generated from template: ./templates/replace_constants.py templates/i2c-bb-base.fs fr2476/i2c-bb-base.tmplfill fr2476/port-regs.fs
+\ generated from template: ./templates/replace_constants.py -i templates/i2c-bb-base.template fr2476/i2c-bb-base.tmplfill fr2476/port-regs.fs
 \ bit-banged i2c driver, base words
 \ There have to be 1..10 kΩ resistors on SDA and SCL to pull them up to idle state.
 \ Master only. Supports clock stretching.
 \ 270 bytes flash
 
 : 0>scl ( -- ) \ drive SCL low
-  %00000010 $0243 cbic! \ (%00000010:SCLPIN) \ (P6OUT:SCLOUT) \ ($0243:P6OUT)
+  %00000010 ( SCLPIN ) $0243 ( P6OUT ) ( SCLOUT ) cbic!
   ;
 
 : 1>scl ( -- ) \ drive SCL high
-  %00000010 $0243 cbis! \ (%00000010:SCLPIN) \ (P6OUT:SCLOUT) \ ($0243:P6OUT)
+  %00000010 ( SCLPIN ) $0243 ( P6OUT ) ( SCLOUT ) cbis!
   ;
 
 : 1>scl-s ( -- ) \ SCL high, stretching allowed
-  %00000010 $0245 cbic! \ SCL input \ (%00000010:SCLPIN) \ (P6DIR:SCLDIR) \ ($0245:P6DIR)
+  %00000010 ( SCLPIN ) $0245 ( P6DIR ) ( SCLDIR ) cbic! \ SCL input
   1>scl  \ clear pulling down
-  begin %00000010 $0241 cbit@ until \ clock stretching to finish \ (%00000010:SCLPIN) \ (P6IN:SCLIN) \ ($0241:P6IN)
-  %00000010 $0245 cbis! \ SCL output \ (%00000010:SCLPIN) \ (P6DIR:SCLDIR) \ ($0245:P6DIR)
+  begin %00000010 ( SCLPIN ) $0241 ( P6IN ) ( SCLIN ) cbit@ until \ clock stretching to finish
+  %00000010 ( SCLPIN ) $0245 ( P6DIR ) ( SCLDIR ) cbis! \ SCL output
   ;
 
 : >sda ( f -- ) \ write f to SDA line
   dup
-  %00000100 $0243 rot if cbis! else cbic! then \ (%00000100:SDAPIN) \ (P6OUT:SDAOUT) \ ($0243:P6OUT)
-  %00000100 $0245 rot if cbic! else cbis! then \ release to input when 1 \ (%00000100:SDAPIN) \ (P6DIR:SDADIR) \ ($0245:P6DIR)
+  %00000100 ( SDAPIN ) $0243 ( P6OUT ) ( SDAOUT ) rot if cbis! else cbic! then
+  %00000100 ( SDAPIN ) $0245 ( P6DIR ) ( SDADIR ) rot if cbic! else cbis! then \ release to input when 1
   ;
 
 : sda> ( -- f ) \ read status of SDA line
-  %00000100 $0241 c@ and 0<> \ (%00000100:SDAPIN) \ (P6IN:SDAIN) \ ($0241:P6IN)
+  %00000100 ( SDAPIN ) $0241 ( P6IN ) ( SDAIN ) c@ and 0<>
   ;
 
 : i2c-init ( -- )  \ initialise bit-banged I2C
-  %00000010 $0245 cbis!  \ SCL output \ (%00000010:SCLPIN) \ (P6DIR:SCLDIR) \ ($0245:P6DIR)
+  %00000010 ( SCLPIN ) $0245 ( P6DIR ) ( SCLDIR ) cbis!  \ SCL output
   1>scl
-  %00000100 $0245 cbic!  \ SDA input \ (%00000100:SDAPIN) \ (P6DIR:SDADIR) \ ($0245:P6DIR)
+  %00000100 ( SDAPIN ) $0245 ( P6DIR ) ( SDADIR ) cbic!  \ SDA input
   ;
 
 : i2c-half ( -- )  \ half-cycle timing delay for I2C
--- msp430/flib/g2553/spi.fs
+++ msp430/flib/g2553/spi.fs
@@ -1,16 +1,16 @@
-\ generated from template: ../templates/replace_constants.py spi.template spi.tmplfill usci-regs.fs port-regs.fs
+\ generated from template: ../templates/replace_constants.py -i spi.template spi.tmplfill usci-regs.fs port-regs.fs
 \ MSP430(G2553) SPI driver
 \   uses USCI_B0 (P1.5 SCK, P1.6 MISO, P1.7 MOSI) {because USCI_A0 is used for USART}
 \   2019 flabbergast
 
-: +spi ( -- ) 1 $29 cbic! ; \ select SPI \ (1:SPIPIN) \ (P2OUT:SPIOUT) \ ($29:P2OUT)
-: -spi ( -- ) 1 $29 cbis! ; \ deselect SPI \ (1:SPIPIN) \ (P2OUT:SPIOUT) \ ($29:P2OUT)
+: +spi ( -- ) 1 ( SPIPIN ) $29 ( P2OUT ) ( SPIOUT ) cbic! ; \ select SPI
+: -spi ( -- ) 1 ( SPIPIN ) $29 ( P2OUT ) ( SPIOUT ) cbis! ; \ deselect SPI
 
 : >spi> ( c -- c )  \ hardware SPI, 1 byte in, 1 byte out
-    begin %1000 $003 bit@ until  \ TXbuf ready? \ (%1000:IFG2_UCB0TXIFG) \ ($003:IFG2)
+    begin %1000 ( IFG2_UCB0TXIFG ) $003 ( IFG2 ) bit@ until  \ TXbuf ready?
     $6F c!  \ send byte (UCB0TXBUF)
-    begin %0100 $003 bit@ until  \ RXbuf ready? \ (%0100:IFG2_UCB0RXIFG) \ ($003:IFG2)
-    $06E c@  \ read byte \ ($06E:UCB0RXBUF)
+    begin %0100 ( IFG2_UCB0RXIFG ) $003 ( IFG2 ) bit@ until  \ RXbuf ready?
+    $06E ( UCB0RXBUF ) c@  \ read byte
     ;
 
 \ single byte shortcuts
@@ -18,13 +18,13 @@
 : >spi ( c -- ) >spi> drop ; \ write byte to SPI
 
 : spi-init ( -- )  \ set up hardware SPI
-    -spi  1 $2A cbis!   \ setup CS pin \ (1:SPIPIN) \ (P2DIR:SPIDIR) \ ($2A:P2DIR)
-    %11100000 $26  cbis!    \ setup SCK|MISO|MOSI pins \ (%11100000:SPISELBITS) \ (P1SEL:SPISEL) \ ($26:P1SEL)
-    %11100000 $41 cbis!    \ setup SCK|MISO|MOSI pins \ (%11100000:SPISELBITS) \ (P1SEL2:SPISEL2) \ ($41:P1SEL2)
-    1 $069 c!               \ put USCI in reset (UCSWRST) \ ($069:UCB0CTL1)
-    %10101001 $068 c!       \ 3-pin 8-bit SPI master (UCCKPH|UCMSB|UCMST|UCSYNC) \ ($068:UCB0CTL0)
-    %11000000 $069 cbis!    \ clock source SMCLK (UCSSELx) \ ($069:UCB0CTL1)
-    2 $06A c! 0 $06B c!   \ prescaler /8 \ ($06B:UCB0BR1) \ ($06A:UCB0BR0)
-    1 $069 cbic!            \ init USCI (UCSWRST) \ ($069:UCB0CTL1)
+    -spi  1 ( SPIPIN ) $2A ( P2DIR ) ( SPIDIR ) cbis!   \ setup CS pin
+    %11100000 ( SPISELBITS ) $26 ( P1SEL ) ( SPISEL )  cbis!    \ setup SCK|MISO|MOSI pins
+    %11100000 ( SPISELBITS ) $41 ( P1SEL2 ) ( SPISEL2 ) cbis!    \ setup SCK|MISO|MOSI pins
+    1 $069 ( UCB0CTL1 ) c!               \ put USCI in reset (UCSWRST)
+    %10101001 $068 ( UCB0CTL0 ) c!       \ 3-pin 8-bit SPI master (UCCKPH|UCMSB|UCMST|UCSYNC)
+    %11000000 $069 ( UCB0CTL1 ) cbis!    \ clock source SMCLK (UCSSELx)
+    2 $06A ( UCB0BR0 ) c! 0 $06B ( UCB0BR1 ) c!   \ prescaler /8
+    1 $069 ( UCB0CTL1 ) cbic!            \ init USCI (UCSWRST)
     ;
 
